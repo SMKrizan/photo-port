63f3f356ac36b2bd80b74fba2ff999fead94d444
SMKrizan <smkrizan@gmail.com>
2021-01-24 13:42:15 -0600

    Nav testing added

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -4,9 +4,12 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
+// note that each test only tests for an individual element
+
 // removes memory data after each test to prevent false results
 afterEach(cleanup);
 
+// note that 'describe()' is not strictly necessary for tests to run; it is used to organize tests into sections that are typically labeled with the element being tested
 describe('About component', () => {
     // renders About tests
     // first test - baseline to verify componenet renders
@@ -17,9 +20,9 @@ describe('About component', () => {
     });
     // second test - test case to compare snapshot (serialized) version of DOM node structure
     it('matches snapshot DOM node structure', () => {
-        // 'asFragment' returns snapshot of About component
+        // Arrange - 'asFragment' returns snapshot of About component
         const { asFragment } = render(<About />);
-        // test and compare whether expected and actual outcomes match
+        // Assert - test and compare whether expected and actual outcomes match
         expect(asFragment()).toMatchSnapshot();
     })
 })
