b04f711df3e260dad36460442ccc2b258bafd6c6
SMKrizan <smkrizan@gmail.com>
2021-01-24 18:05:20 -0600

    gallery conditionally rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
+  // initializing category state and using 'useState()' hook to enable category change
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    {
+      name: 'portraits',
+      description: 'Portraits of people in my life',
+    },
+    {
+      name: 'food',
+      description: 'Delicious delicacies',
+    },
+    {
+      name: 'landscape',
+      description: 'fields, farmhouses, waterfalls, and the beauty of nature',
+    },
+  ]);
+
+  // first parameter is value of state and second is a setter which allows state change; then passing initial state '0' to useState(), which always returns an array
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   // helpful to think of JSX injection as a function that uses "document.createElement(JSX)"; the way React uses JSX behind the scenes is similar to document.createElement()
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <About>
-
-        </About>
+        <div>
+        <Gallery></Gallery>
+        <About></About>
+        </div>
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,30 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 
-const Nav = () => {
-    function categorySelected(name) {
-        console.log(`${name} clicked`)
-    };
-    const categories = [
-        {
-            name: 'Commercial',
-            description: 'Photos of grocery stores, food trucks, and other commercial projects',
-        },
-        {
-            name: 'Portraits',
-            description: 'Portraits of people in my life',
-        },
-        {
-            name: 'Food',
-            description: 'Delicious delicacies',
-        },
-        {
-            name: 'Landscape',
-            description: 'fields, farmhouses, waterfalls, and the beauty of nature',
-        },
-    ];
+const Nav = (props) => {
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory
+    } = props;
+
+    // useEffect() is an API reflecting lifecycle methods of component
+
+    // invoking useEffect() and assigning DOM node in the fn body of the cb fn, where 1st argument 'capitalizeFirstLetter(currentCategory.name)' is cb fn and 2nd argument '[currentCategory]' is array with single element 'currentCategory'; this 2nd argument directs hook to re-render component on changes to state value --> e.g. if currentCategory changes, component will re-render so that user sees change in document.title
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name);
+    }, [currentCategory]);    
+    
     return (
-        <header>
+        <header className='flex-row px-1'>
             <h2>
                 <a data-testid='link' href='/'>
                     <span role='img' aria-label='camera'> 📸</span> Oh Snap!
@@ -33,7 +26,7 @@ const Nav = () => {
             <nav>
                 <ul className='flex-row'>
                     <li className='mx-2'>
-                        <a data-testid='about'href='#about'>
+                        <a data-testid='about' href='#about'>
                             About me
                         </a>
                     </li>
@@ -42,11 +35,12 @@ const Nav = () => {
                     </li>
                     {/* whenever we map over anything in JSX, the outermost element must have a unique 'key' attribute, which helps React keep track of items in the virtual DOM */}
                     {categories.map((category) => (
-                        <li className='mx-1'
-                        key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)}>
-                                {category.name}
+                        <li className={`mx-1 ${
+                            currentCategory.name === category.name && 'navActive'
+                        }`} key={category.name}>
+                            <span onClick={() => {
+                                setCurrentCategory(category)}}>
+                                {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
                     ))}
